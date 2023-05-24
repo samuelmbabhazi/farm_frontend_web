d7c496a8d8c3e84ac6338c1d5749f66d4b0f703f
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2023-05-24 22:03:14 +0200

    implemente la page de connexion

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import "./App.css";
 import OnboardingScreen from "./pages/Onboarding";
 
 import Getstart from "./pages/Getstart";
+import LoginForm from "./pages/Login";
 
 function App() {
+  function onLogin(email, password) {
+    // Faites quelque chose avec l'email et le mot de passe, par exemple vérifiez-les contre une base de données ou un serveur d'authentification tiers
+    console.log(email, password);
+  }
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="*" element={<OnboardingScreen />} />
           <Route path="/getstart" element={<Getstart />} />
+          <Route path="/login" element={<LoginForm onLogin={onLogin} />} />
         </Routes>
       </BrowserRouter>
     </>
